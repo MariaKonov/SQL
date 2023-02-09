6fbf37e9d0f1b4e8c7967b4b572e4deb3fb6e72f
Maria <marijka000@gmail.com>
2023-02-09 20:53:40 +0300

    New

---
+++ src/test/java/page/DashboardPage.java
@@ -0,0 +1,15 @@
+package page;
+
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+
+public class DashboardPage {
+    private SelenideElement headind = $("[data-test-id='dashboard]");
+
+    public DashboardPage() {
+        headind.shouldBe(visible);
+    }
+}
