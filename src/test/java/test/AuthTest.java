c0f59afb2274019ae7bdbd1b07d197eb8acb39e2
Maria <marijka000@gmail.com>
2023-03-14 10:42:13 +0300

    test

--- src/test/java/test/AuthTest.java
+++ src/test/java/test/AuthTest.java
@@ -43,11 +43,11 @@ public class AuthTest {
     @DisplayName("Should get error notification if login with random exist in base and active user and random verification code")
     void ShouldGetErrorNotificationIfLoginWithRandomExistInBaseAndActiveUserAndRandomVerificationCode() {
         var loginPage = open("http://localhost:9999", LoginPage.class);
-        var authInfo = DataHelper.generateRandomUser();
+        var authInfo = DataHelper.getAuthInfoUseTestData();
         var verificationPage = loginPage.validLogin(authInfo);
         verificationPage.verifyVerificationPageVisiblity();
-        var verificationCode = SQLHelper.getVerificationCode();
-        verificationPage.validVerify(verificationCode.getCode());
+        var verificationCode = DataHelper.generateRandomVerificationCode();
+        verificationPage.verify(verificationCode.getCode());
         verificationPage.verifyErrorNotificationVisiblity();
     }
 }
